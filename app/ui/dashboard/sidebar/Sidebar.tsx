666c6bf4504d8ad0dff0ebc0a5069cefd21bf37a
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-06-24 22:34:03 +0500

    Working on the Side bar

--- app/ui/dashboard/sidebar/Sidebar.tsx
+++ app/ui/dashboard/sidebar/Sidebar.tsx
@@ -1,90 +1,105 @@
-import React from 'react'
-import style from './sidebar.module.css'
-import { MdSupervisedUserCircle , MdDashboard , MdShoppingBag , MdAttachMoney, MdWork, MdAnalytics, MdPeople, MdOutlineSearch, MdOutlineSettings, MdHelpCenter} from 'react-icons/md'
-import MenuLink from './MenuLink/MenuLink'
+import React from "react";
+import style from "./sidebar.module.css";
+import {
+  MdSupervisedUserCircle,
+  MdDashboard,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSearch,
+  MdOutlineSettings,
+  MdHelpCenter,
+} from "react-icons/md";
+import MenuLink from "./MenuLink/MenuLink";
+import Image from "next/image";
 
 const menuItems = [
-    {
-        title : 'Pages',
-        list : [
-            {
-                title : 'Dashboard',
-                path : '/dashboard',
-                icon : <MdDashboard/>
-            },
-            {
-                title : 'Users',
-                path : '/dashboard/users',
-                icon : <MdSupervisedUserCircle/>
-            },
-            {
-                title : 'Products',
-                path : '/dashboard/products',
-                icon : <MdShoppingBag/>
-            },
-            {
-                title : 'Transactions',
-                path : '/dashboard/transactions',
-                icon : <MdAttachMoney/>
-            }
-        ]
-    },
-    {
-        title : 'Analytics',
-        list: [
-            {
-                title : 'Revenue',
-                path : '/dashboard/revenue',
-                icon : <MdWork/>
-            }, 
-            {
-                title : 'Reports',
-                path : '/dashboard/reports',
-                icon : <MdAnalytics/>
-            },
-            {
-                title : 'Teams',
-                path : '/dashboard/teams',
-                icon : <MdPeople/>
-            }
-            
-        ]
-    },
-    {
-        title : "User",
-        list : [
-            {
-                title : 'Settings',
-                path : '/dashboard/settings',
-                icon : <MdOutlineSettings/>
-            },
-            {
-                title : 'Help',
-                path : '/dashboard/help',
-                icon : <MdHelpCenter/ >
-            }
-        ]
-    }
-]
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: "Analytics",
+    list: [
+      {
+        title: "Revenue",
+        path: "/dashboard/revenue",
+        icon: <MdWork />,
+      },
+      {
+        title: "Reports",
+        path: "/dashboard/reports",
+        icon: <MdAnalytics />,
+      },
+      {
+        title: "Teams",
+        path: "/dashboard/teams",
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Settings",
+        path: "/dashboard/settings",
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: "Help",
+        path: "/dashboard/help",
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
 
 const Sidebar = () => {
   return (
     <div className={style.container}>
-        <ul>
-        {menuItems.map((category)=>(
-        <li key={category.title}>
-            <span>{category.title}</span>
-            {category.list.map((item)=>(
-                <MenuLink key={item.title} items={item}></MenuLink>
+      <div className={style.user}>
+        <Image src="/noavatar.png" alt="" width="50" height="50"></Image>
+        <div className={style.userDetail}>
+        <span className={style.username}>Abdullah </span>
+        <span className={style.userTitle}>Administrator</span>
+      </div>
+    </div>
+      <ul>
+        {menuItems.map((category) => (
+          <li key={category.title}>
+            <span className={style.category}>{category.title}</span>
+            {category.list.map((item) => (
+              <MenuLink key={item.title} items={item}></MenuLink>
             ))}
-        </li>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-    ))}
-
-        </ul>
-        
-        </div>
-  )
-}
-
-export default Sidebar
+export default Sidebar;
