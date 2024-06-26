7ec6047aed1108ab009ffdac2279c44c1e6946be
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-06-26 16:24:52 +0500

    chart is created

--- app/ui/dashboard/chart/Chart.tsx
+++ app/ui/dashboard/chart/Chart.tsx
@@ -1,9 +1,63 @@
+'use client'
 import React from 'react'
 import style from './chart.module.css'
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
+
+const data : {name : string , visit : number , click : number}[] = [
+  {
+    name : 'Mon',
+    visit : 3000,
+    click : 1398
+  },
+  {
+    name : 'Tue',
+    visit : 2000,
+    click : 3800
+  },{
+    name : 'Wed',
+    visit : 2780,
+    click : 3908
+  },{
+    name : 'Thu',
+    visit : 1890,
+    click : 4800
+  },{
+    name : 'Fri',
+    visit : 2390,
+    click : 3800
+  },{
+    name : 'Sat',
+    visit : 3490,
+    click : 4300
+  }
+]
 const Chart = () => {
   return (
-    <div className={style.container} >Chart</div>
+    <div className={style.container} >
+      <h2 className={style.title}>Weekly Recap</h2>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+         {/* <CartesianGrid strokeDasharray="3 3" /> */}
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="visit" stroke="#8884d8" strokeDasharray="5 5" />
+          <Line type="monotone" dataKey="click" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
