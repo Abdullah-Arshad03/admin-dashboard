a555d19ef0e03883bd8e8b2b8934b9e868f4f089
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-06-27 20:12:59 +0500

    add user and add product pages are done

--- app/dashboard/users/add/page.tsx
+++ app/dashboard/users/add/page.tsx
@@ -1,25 +1,42 @@
-import React from 'react'
-import style from '@/app/ui/dashboard/users/addUser/addUser.module.css'
+import React from "react";
+import style from "@/app/ui/dashboard/users/addUser/addUser.module.css";
+
 
 const AddUserPage = () => {
   return (
     <div className={style.container}>
-        <form action="" className={style.form}>
-            <input type="text" placeholder='username' id='username' name='username' />
-            <input type="email" placeholder='email' id='email' name='email' />
-            <input type="password" placeholder='password' id='password' name='password' />
-            <input type="number" placeholder='phone' id='phone' name='phone' />
-            <select name="" id="">
-            <option value="general" >Admin Or User ?</option>
+      <form action="" className={style.form}>
+        <input className={style.input}
+          type="text"
+          placeholder="username"
+          id="username"
+          name="username"
+        />
+        <input className={style.input} type="email" placeholder="email" id="email" name="email" />
+        <input
+          className={style.input}
+          type="password"
+          placeholder="password"
+          id="password"
+          name="password"
+        />
+        <input className={style.input} type="number" placeholder="phone" id="phone" name="phone" />
 
-                <option value="isAdmin" >isAdmin</option>
-                <option value="isUser" >isUser</option>
-            </select>
-
-        </form>
+        <select className={style.input} name="isAdmin" id="isAdmin">
+          <option value="general">isAdmin?</option>
+          <option value="Admin">Yes</option>
+          <option value="notAdmin">No</option>
+        </select>
+        <select className={style.input} name="isActive" id="isActive">
+          <option value="general">isActive?</option>
+          <option value="Active">Yes</option>
+          <option value="notActive">No</option>
+        </select>
+        <textarea  name="Address" id="Address" placeholder="Address" rows={16}  className={style.input}></textarea>
+        <button type='submit' className={style.submit}>Submit</button>
+      </form>
     </div>
-  )
+  );
+};
 
-}
-
-export default AddUserPage
+export default AddUserPage;
