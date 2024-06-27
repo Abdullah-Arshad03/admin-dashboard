d6fd4fdba5f8461b57edec030bef8176033b0e56
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-06-27 22:57:10 +0500

    login done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from "react";
+import style from "./login.module.css";
 
 const Login = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
-export default Login
+    <div className={style.container}>
+      <form className={style.form}>
+        <label>Email</label>
+        <input className={style.input} type="Email" placeholder="Enter Email" />
+
+        <label>Password</label>
+        <input  className={style.input} type="password" placeholder="Enter Password" />
+
+        <button  type="submit" className={style.submit}>
+          submit
+        </button>
+        <p></p>
+      </form>
+    </div>
+  );
+};
+export default Login;
